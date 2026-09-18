207401f6ee6125c9f57a750d3756e77aab04cf4c
nyaruku <38096703+nyaruku@users.noreply.github.com>
2026-09-18 22:52:22 +0200

    Remove access_log entirely

--- src/Migration/Database.js
+++ src/Migration/Database.js
@@ -1,7 +1,6 @@
 import { migrateDownloadStats } from './Tables/download_stats.js';
 import { migrateRequestBatch } from './Tables/request_batch.js';
 import { migrateRequestItem } from './Tables/request_item.js';
-import { migrateAccessLog } from './Tables/access_log.js';
 import { migrateApiKeys } from './Tables/api_keys.js';
 
 export async function runMigrations(pool) {
