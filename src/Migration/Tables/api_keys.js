377fd8b5dfdc05a8c8e227ed6440e8a53b46d694
nyaruku <38096703+nyaruku@users.noreply.github.com>
2026-09-18 02:00:49 +0200

    Fix api_keys sql

--- src/Migration/Tables/api_keys.js
+++ src/Migration/Tables/api_keys.js
@@ -2,8 +2,8 @@ export async function migrateApiKeys(pool) {
     await pool.query(`
         CREATE TABLE IF NOT EXISTS api_keys (
             api_key TEXT
-            discord_uid TEXT
-            osu_uid BIGINT
+            ,discord_uid TEXT
+            ,osu_uid BIGINT
         );
     `);
     console.log('api_keys table is ready');
